f8e6f9780cddc7cf2883ee8c5198f4579f43e695
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-09-12 23:52:45 +0600

    from mongodb data all get, single data get

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ async function run(){
     try{
         await client.connect();
         const bookCollection = client.db("bestSellerbooks").collection("bestSeller");
+        const uposohokariCollection = client.db("bestSellerbooks").collection("uposohokari");
+        const nonTechCollection = client.db("bestSellerbooks").collection("nonTech");
+        const sohokariCollection = client.db("bestSellerbooks").collection("sohokari");
+        const admissionCollection = client.db("bestSellerbooks").collection("admission");
+        const treadCollection = client.db("bestSellerbooks").collection("treadCourse");
 
        // get bestSeller books
       app.get('/bestseller', async(req, res) =>{
@@ -26,6 +31,109 @@ async function run(){
         const bestSellerBooks = await cursor.toArray();
         res.send(bestSellerBooks);
       })
+
+      app.get('/productDetails/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('id',id)
+        const query = {_id:ObjectId(id)};
+        const result =await bookCollection.findOne(query);
+        console.log(result)
+          res.send(result)
+  
+    })
+
+    // get uposohokary book
+
+    app.get('/uposohokari', async(req, res) =>{
+        const query = {};
+        const cursor = uposohokariCollection.find(query);
+        const uposohokariBooks = await cursor.toArray();
+        res.send(uposohokariBooks);
+      })
+
+      app.get('/product2Details/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('id',id)
+        const query = {_id:ObjectId(id)};
+        const result =await uposohokariCollection.findOne(query);
+        console.log(result)
+          res.send(result)
+  
+    })
+
+    // get non-tech book
+
+    app.get('/nonTech', async(req, res) =>{
+        const query = {};
+        const cursor = nonTechCollection.find(query);
+        const nonTechBooks = await cursor.toArray();
+        res.send(nonTechBooks);
+      })
+
+      app.get('/product3Details/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('id',id)
+        const query = {_id:ObjectId(id)};
+        const result =await nonTechCollection.findOne(query);
+        console.log(result)
+          res.send(result)
+  
+    })
+    // get sohokari book
+
+    app.get('/sohokari', async(req, res) =>{
+        const query = {};
+        const cursor = sohokariCollection.find(query);
+        const sohokariBooks = await cursor.toArray();
+        res.send(sohokariBooks);
+      })
+
+      app.get('/product4Details/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('id',id)
+        const query = {_id:ObjectId(id)};
+        const result =await sohokariCollection.findOne(query);
+        console.log(result)
+          res.send(result)
+  
+    })
+    // get Admission book
+
+    app.get('/admission', async(req, res) =>{
+        const query = {};
+        const cursor = admissionCollection.find(query);
+        const admissionBooks = await cursor.toArray();
+        res.send(admissionBooks);
+      })
+
+      app.get('/product5Details/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('id',id)
+        const query = {_id:ObjectId(id)};
+        const result =await admissionCollection.findOne(query);
+        console.log(result)
+          res.send(result)
+  
+    })
+    // get treadCourse book
+
+    app.get('/treadCourse', async(req, res) =>{
+        const query = {};
+        const cursor = treadCollection.find(query);
+        const sohokariBooks = await cursor.toArray();
+        res.send(sohokariBooks);
+      })
+
+      app.get('/product6Details/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('id',id)
+        const query = {_id:ObjectId(id)};
+        const result =await treadCollection.findOne(query);
+        console.log(result)
+          res.send(result)
+  
+    })
+
     }
     finally{
 
