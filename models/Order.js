eebd50b2cf76c785f15265e4ac114e2332de49ec
Mohammad Al Amin <alaminbamna08@gmail.com>
2023-05-29 01:14:37 +0600

    order model update

--- models/Order.js
+++ models/Order.js
@@ -1,75 +1,81 @@
 const mongoose = require("mongoose");
-const {ObjectId} = mongoose.Schema.Types;
+const { ObjectId } = mongoose.Schema.Types;
 // Schema Design
-const orderSchema = mongoose.Schema({
-    orderItems:{
-        type:Array,
-        required:true
+const orderSchema = mongoose.Schema(
+  {
+    orderItems: {
+      type: Array,
+      required: true,
     },
-    user:{
-        type:String,
-        required:true,
+    user: {
+      type: String,
+      required: true,
     },
-    name:{
-        type:String,
-        required:[true, 'Please Provide Your Name'],
+    name: {
+      type: String,
+      required: [true, "Please Provide Your Name"],
     },
-   
-    email:{
-        type:String,
-        required:[true, 'Please Provide Your Email'],
-        
-    },
-    phone:{
-        type:String,
-        required:[true, 'Please Provide Your Phone Number'],
-    },
-    country:{
-        type:String,
-    },
-    city:{
-        type:String,
-        required:[true, 'Please Provide Your City Name'],
-    },
-    zip:{
-        type:String,
-        required:[true, 'Please Provide Your Zip Code'],
-    },
-    address:{
-        type:String,
-        required:[true, 'Please Provide Your Address'],
-    },
-    district:{
-        type:String,
-        required:[true, 'Please Provide Your District Name'],
-    },
-    paymentType:{
-        type:String,
-        required:[true, 'Please Provide Your Payment Type'],
-    },
-    bkashNumber:{
-        type:String,
-    },
-    bkashTrx:{
-        type:String,
-    },
-    nagadNumber:{
-        type:String,
-    },
-    nagadTrx:{
-        type:String,
-    },
-    orderStatus:{
-        type:String,
-        enum:['Pending','Approved','Cancelled'],
-        default:"Pending"
-    },
-    
-},
-{
-    timestamps:true
-})
 
+    email: {
+      type: String,
+      required: [true, "Please Provide Your Email"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please Provide Your Phone Number"],
+    },
+    country: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: [true, "Please Provide Your City Name"],
+    },
+    zip: {
+      type: String,
+      required: [true, "Please Provide Your Zip Code"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please Provide Your Address"],
+    },
+    district: {
+      type: String,
+      required: [true, "Please Provide Your District Name"],
+    },
+    paymentType: {
+      type: String,
+      required: [true, "Please Provide Your Payment Type"],
+    },
+    bkashNumber: {
+      type: String,
+    },
+    bkashTrx: {
+      type: String,
+    },
+    nagadNumber: {
+      type: String,
+    },
+    nagadTrx: {
+      type: String,
+    },
+    dutchNumber: {
+      type: String,
+    },
+    dutchTrx: {
+      type: String,
+    },
+
+    orderStatus: {
+      type: String,
+      enum: ["Pending", "Approved", "Cancelled"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // middleware
 // productSchema.pre('save', function(next){
@@ -83,6 +89,6 @@ const orderSchema = mongoose.Schema({
 // console.log(`this product ${this.name} added`)
 // }
 // Model
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
